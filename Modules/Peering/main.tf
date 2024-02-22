d86285932a6e31bfceb703824ab02b1ff812156d
Robert-J-Barros <robert.junior.goncalves@gmail.com>
2024-02-22 01:27:00 -0300

    file

--- Modules/Peering/main.tf
+++ Modules/Peering/main.tf
@@ -1,47 +1,70 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.92.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "myResourceGroup"
+    storage_account_name = "solutiononetfstate"
+    container_name       = "network-tfstate"
+    key                  = "prod.terraform.tfstate"
+}
+}
 
-data "azurerm_resource_group" "ResourceGroupSolutionOneFase2" {
-  name = var.resource_group_name
-}
-data "azurerm_virtual_network" "VnetSolutionOneFase" {
-  name                = var.vnet_application_gateway
-  resource_group_name = var.resource_group_name
-}
-data "azurerm_public_ip" "IpNatGatewaySolutionOneFase2" {
-  name                = var.public_ip_nat_name
-  resource_group_name = var.resource_group_name
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_virtual_network_peering" "peering_nat_to_aks" {
-  name                         = "${data.azurerm_virtual_network.ResourceGroupSolutionOneFase2.resource_group_name}-nat"
-  resource_group_name          = data.azurerm_virtual_network.ResourceGroupSolutionOneFase2.resource_group_name
-  virtual_network_name         = var.peering_aks_name
-  remote_virtual_network_id    = var.peering_aks_id
+data "azurerm_subnet" "aks_subnet" {
+  name                 = var.subnet_aks_name
+  virtual_network_name = var.vnet_peering_aks_nat_name
+  resource_group_name  = var.aks_resource_group_name
+}
+data "azurerm_virtual_network" "AKSVnet" {
+  name                = var.vnet_peering_aks_nat_name
+  resource_group_name = var.aks_resource_group_name
+}
+
+data "azurerm_virtual_network" "NATVnet" {
+  name                = var.vnet_peering_nat_aks_name
+  resource_group_name = var.nat_resource_group_name
+}
+
+
+resource "azurerm_virtual_network_peering" "PeeringNatToKksSolutionOne" {
+  name                         = var.peering_nat_to_aks
+  resource_group_name          = var.nat_resource_group_name
+  virtual_network_name         = var.vnet_peering_nat_aks_name
+  remote_virtual_network_id    = data.azurerm_virtual_network.AKSVnet.id
   allow_virtual_network_access = true
 }
 
 resource "azurerm_virtual_network_peering" "peering_aks_to_nat" {
-  name                         = "${data.azurerm_virtual_network.ResourceGroupSolutionOneFase2.resource_group_name}-aks"
-  resource_group_name          = data.azurerm_virtual_network.ResourceGroupSolutionOneFase2.resource_group_name
-  virtual_network_name         = var.peering_nat_name
-  remote_virtual_network_id    = var.peering_nat_id
+  name                         = var.peering_aks_to_nat
+  resource_group_name          = var.aks_resource_group_name
+  virtual_network_name         = var.vnet_peering_aks_nat_name
+  remote_virtual_network_id    = data.azurerm_virtual_network.NATVnet.id
   allow_virtual_network_access = true
 }
 
 
 resource "azurerm_route_table" "route_table_aks" {
-  name                = "route-table-${data.azurerm_virtual_network.ResourceGroupSolutionOneFase2.name}"
-  location            = data.azurerm_virtual_network.ResourceGroupSolutionOneFase2.location
-  resource_group_name = data.azurerm_virtual_network.ResourceGroupSolutionOneFase2.name
+  name                = var.route_table
+  location            = var.route_table_region
+  resource_group_name = var.aks_resource_group_name
 
   route {
-    name                = "route-to-internet"
+    name                = "routetointernet"
     address_prefix      = "0.0.0.0/0"
     next_hop_type       = "VirtualAppliance"
-    next_hop_in_ip_address = azurerm_subnet.subnet_nat.private_ip_address
+    next_hop_in_ip_address = "172.31.0.0/24"
   }
 }
 
 resource "azurerm_subnet_route_table_association" "route_table_association_aks" {
-  subnet_id      = azurerm_subnet.subnet_aks.id
+  subnet_id      = data.azurerm_subnet.aks_subnet.id
   route_table_id = azurerm_route_table.route_table_aks.id
 }
+
--- Modules/Peering/variables.tf
+++ Modules/Peering/variables.tf
@@ -1,40 +1,60 @@
-variable "resource_group_name" {
+##PEERING NAT VNET TO AKS VNET
+
+variable "peering_nat_to_aks" {
+  description = "Name of the peering"
+  default     = "PeeringNatToKksSolutionOne"
+}
+
+variable "nat_resource_group_name" {
   description = "Name of the resource group."
   default     = "ResourceGroupSolutionOneFase2"
 }
 
-variable "public_ip_nat_name" {
-  description = "Name of Ip association NAT"
-  default     = ""
+variable "vnet_peering_nat_aks_name" {
+  description = "Name of vnet."
+  default     = "VnetSolutionOneFase"
+}
+
+variable "vnet_peering_aks_nat_id" {
+  description = "Id of vnet AKS."
+  default     = "/subscriptions/5dedbf86-9743-4904-b918-8b1ed28a138e/resourceGroups/ResourceGroupSolutionOneFase2-aks/providers/Microsoft.Network/virtualNetworks/aks-vnet-25883643/subnets/aks-subnet"
 }
 
 
-#### APPLICATION GATEWAY
+#######
 
-variable "vnet_application_gateway" {
+##PEERING AKS VNET TO NAT VNET
+variable "peering_aks_to_nat" {
+  description = "Name of the peering"
+  default     = "PeeringAKSToNATSolutionOne"
+}
+variable "aks_resource_group_name" {
   description = "Name of the resource group."
-  default     = ""
+  default     = "ResourceGroupSolutionOneFase2-aks"
+}
+variable "vnet_peering_aks_nat_name" {
+  description = "Name of vnet."
+  default     = "aks-vnet-25883643"
+}
+variable "vnet_peering_nat_aks_id" {
+  description = "Id of vnet NAT."
+  default     = "/subscriptions/5dedbf86-9743-4904-b918-8b1ed28a138e/resourceGroups/ResourceGroupSolutionOneFase2/providers/Microsoft.Network/virtualNetworks/VnetSolutionOneFase/subnets/SubnetPubSolutionOneFase2"
 }
 
-variable "peering_nat_name" {
-  description = "Name of the resource group."
-  default     = ""
-}
-variable "peering_aks_id" {
-  description = "Name of the resource group."
-  default     = ""
-}
+############
 
-### AKS
-
-variable "peering_aks_name" {
-  description = "Name of the resource group."
-  default     = ""
+##ROUTE TABLE
+variable "route_table" {
+  description = "Name of route table."
+  default     = "routetablenat"
 }
 
-variable "peering_nat_id" {
-  description = "Name of the resource group."
-  default     = ""
+variable "route_table_region" {
+  description = "Route table region"
+  default     = "East US"
 }
 
-
+variable "subnet_aks_name" {
+  description = "name subnet aks"
+  default     = "aks-subnet"
+}
